13b0a5f408e86fe9b2a28bf22187a62656e210a2
alexandregama <alexandre.gama.lima@gmail.com>
2015-08-04 20:48:22 -0300

    Refresh puppet file to install zip

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,8 +1,13 @@
 exec { "apt-update":
-	command => "/usr/bin/apt-get update"
+	command => "/usr/bin/apt-get update"	
 }
 
 package { ["openjdk-7-jre", "tomcat7"]:
+	ensure => installed,
+	require => Exec["apt-update"]	
+}
+
+package { "zip":
 	ensure => installed,
 	require => Exec["apt-update"]	
 }
